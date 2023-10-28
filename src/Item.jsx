659ad0bd573dea6685ff8407edde615e85ff7832
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2023-10-28 18:13:36 +0200

    :construction: Modification  :construction:

--- src/Item.jsx
+++ src/Item.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-export const Item = () => {
+export const Item = ({item, index}) => {
   return (
-    <div>Item</div>
+    <div><small style={{marginRight: "3px"}}>{index}. </small>{item.charAt(0).toUpperCase()+ item.slice(1)}</div>
   )
 }
